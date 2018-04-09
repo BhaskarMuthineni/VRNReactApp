9be0b59d41b39c749c9724de0a082bd49bd89fab
Bhaskar <bhaskarmuthineni@gmail.com>
2018-04-09 19:18:05 +0530

    form design changes

--- App.jsx
+++ App.jsx
@@ -16,10 +16,17 @@ import Button from 'material-ui/Button';
 import AddIcon from 'material-ui-icons/Add';
 import SearchIcon from 'material-ui-icons/Search';
 import Select from 'material-ui/Select';
-import { FormControl, FormHelperText } from 'material-ui/Form';
+import { FormLabel, FormControl, FormControlLabel, FormHelperText } from 'material-ui/Form';
 import { MenuItem } from 'material-ui/Menu';
 import Input, { InputLabel } from 'material-ui/Input';
 import Tabs, { Tab } from 'material-ui/Tabs';
+import ExpansionPanel, {
+    ExpansionPanelSummary,
+    ExpansionPanelDetails,
+  } from 'material-ui/ExpansionPanel';
+import ExpandMoreIcon from 'material-ui-icons/ExpandMore';
+import TextField from 'material-ui/TextField';
+import Radio, { RadioGroup } from 'material-ui/Radio';
 
 const drawerWidth = 290;
 
@@ -74,11 +81,27 @@ const styles = theme => ({
   },
   formControl: {
     margin: theme.spacing.unit,
-    minWidth: 120,
+    width: '100%'
   },
   selectEmpty: {
     marginTop: theme.spacing.unit * 2,
   },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    width: '100%'
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+  },
+  group: {
+      margin: `${theme.spacing.unit}px 0`,
+  }
 });
 
 class App extends React.Component {
@@ -86,13 +109,19 @@ class App extends React.Component {
         super(props);
         this.state = {
             mobileOpen: false,
+            tabValue : 0
         };
         this.handleDrawerToggle = this.handleDrawerToggle.bind(this);
+        this.handleTabChange = this.handleTabChange.bind(this);
     }
 
     handleDrawerToggle() {
         this.setState({ mobileOpen: !this.state.mobileOpen });
     };
+
+    handleTabChange(event, value) {
+        this.setState({ tabValue: value });
+    }
 
     render() {
         const { classes, theme } = this.props;
@@ -100,7 +129,7 @@ class App extends React.Component {
         return (
             <div className={classes.root}>
                 <Master classes={classes} theme={theme} handleDrawerToggle={this.handleDrawerToggle} mobileOpen={this.state.mobileOpen}/>
-                <Detail classes={classes} theme={theme} handleDrawerToggle={this.handleDrawerToggle}/>
+                <Detail classes={classes} theme={theme} handleDrawerToggle={this.handleDrawerToggle} tabValue={this.state.tabValue} handleTabChange={this.handleTabChange}/>
             </div>
         );        
     }
@@ -179,7 +208,6 @@ class Master extends React.Component {
 class Detail extends React.Component {
     render() {
         const { classes, theme } = this.props;
-        this.props.value = 0;
 
         return (
             <div style={{ width: '100%' }}>
@@ -200,7 +228,7 @@ class Detail extends React.Component {
                 </AppBar>
                 <main className={classes.content}>
                 <div className={classes.toolbar} />
-                <FormControl className={classes.formControl} style={{ width: '100%'}}>
+                <FormControl className={classes.formControl}>
                     <InputLabel htmlFor="age-simple">Mode of Transport</InputLabel>
                     <Select value={'CA'}>
                         <MenuItem value={'CA'}>Courier Air</MenuItem>
@@ -210,10 +238,170 @@ class Detail extends React.Component {
                         <MenuItem value={'RD'}>Road Truck</MenuItem>
                     </Select>
                 </FormControl>
-                <Tabs value={this.props.value} onChange={this.props.value = 1}>
-                    <Tab label="Arrival" />
-                    <Tab label="Departure" />
-                </Tabs>
+                <AppBar position="static">
+                    <Tabs value={this.props.tabValue} onChange={this.props.handleTabChange}>
+                        <Tab label="Arrival">                        
+                        </Tab>
+                        <Tab label="Departure" />
+                    </Tabs>
+                </AppBar>
+                {
+                    this.props.tabValue === 0 &&
+                    <div>
+                        <ExpansionPanel>
+                            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                                <Typography className={classes.heading}>Vehicle</Typography>
+                            </ExpansionPanelSummary>
+                            <ExpansionPanelDetails>
+                                <form className={classes.container} noValidate autoComplete="off">                                    
+                                    <TextField
+                                    id="name"
+                                    label="Vehicle Status"
+                                    className={classes.textField}
+                                    value="Loaded"                                    
+                                    margin="normal"
+                                    />
+                                    <TextField
+                                    id="name"
+                                    label="Vehicle No."
+                                    className={classes.textField}
+                                    value="MH01R1234"                                    
+                                    margin="normal"
+                                    />
+                                    <TextField
+                                    id="name"
+                                    label="Fleet Type"
+                                    className={classes.textField}
+                                    value="Market Vehicle"                                    
+                                    margin="normal"
+                                    />
+                                    <TextField
+                                    id="name"
+                                    label="Transporter/Agency Name"
+                                    className={classes.textField}
+                                    value="Test Vendor"                                    
+                                    margin="normal"
+                                    />
+                                    <TextField
+                                    id="name"
+                                    label="Seal Condition"
+                                    className={classes.textField}
+                                    value="Intact"                                    
+                                    margin="normal"
+                                    />
+                                    <TextField
+                                    id="name"
+                                    label="Seal No. 1"
+                                    className={classes.textField}
+                                    value="1123"                                    
+                                    margin="normal"
+                                    />
+                                    <TextField
+                                    id="name"
+                                    label="Seal No. 2"
+                                    className={classes.textField}
+                                    value="123"                                    
+                                    margin="normal"
+                                    />
+                                    <TextField
+                                    id="name"
+                                    label="No. of Boxes"
+                                    className={classes.textField}
+                                    value="10"                                    
+                                    margin="normal"
+                                    />
+                                </form>
+                            </ExpansionPanelDetails>
+                        </ExpansionPanel>
+                        <ExpansionPanel>
+                            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                            <Typography className={classes.heading}>Driver</Typography>
+                            </ExpansionPanelSummary>
+                            <ExpansionPanelDetails>
+                                <form className={classes.container} noValidate autoComplete="off">                                    
+                                    <TextField
+                                    id="name"
+                                    label="Driver License No."
+                                    className={classes.textField}
+                                    value="LIC1234"                                    
+                                    margin="normal"
+                                    />
+                                    <TextField
+                                    id="name"
+                                    label="Mobile No."
+                                    className={classes.textField}
+                                    value="987654"                                    
+                                    margin="normal"
+                                    />
+                                    <TextField
+                                    id="name"
+                                    label="Driver/Pickup Person Name"
+                                    className={classes.textField}
+                                    value="RAM"                                    
+                                    margin="normal"
+                                    />
+                                </form>
+                            </ExpansionPanelDetails>
+                        </ExpansionPanel>
+                        <ExpansionPanel>
+                            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                                <Typography className={classes.heading}>Remarks</Typography>
+                            </ExpansionPanelSummary>
+                            <ExpansionPanelDetails>
+                                <form className={classes.container} noValidate autoComplete="off">                                    
+                                    <TextField
+                                    id="name"
+                                    label="AWB No./Docker No./LR No."
+                                    className={classes.textField}
+                                    value="asdf"                                    
+                                    margin="normal"
+                                    />
+                                    <TextField
+                                    id="name"
+                                    label="In Remarks"
+                                    className={classes.textField}
+                                    value="asdf"                                    
+                                    margin="normal"
+                                    />                                    
+                                </form>
+                            </ExpansionPanelDetails>
+                        </ExpansionPanel>
+                    </div>
+                }
+                {
+                    this.props.tabValue === 1 &&
+                    <div>
+                        <form className={classes.container} noValidate autoComplete="off">
+                            <FormControl component="fieldset" required className={classes.formControl}>
+                                <RadioGroup aria-label="Vehicle Status" name="VehStat" className={classes.group} value="L">
+                                    <FormControlLabel value="L" control={<Radio />} label="Loaded"/>
+                                    <FormControlLabel value="E" control={<Radio />} label="Empty"/>
+                                </RadioGroup>
+                            </FormControl>
+                            <TextField
+                            id="name"
+                            label="No. of Boxes"
+                            className={classes.textField}
+                            value="asdf"                                    
+                            margin="normal"
+                            />
+                            <FormControl component="fieldset" required className={classes.formControl}>
+                                <RadioGroup aria-label="Seal Condition" name="SealCond" className={classes.group} value="I">
+                                    <FormControlLabel value="I" control={<Radio />} label="Intact"/>
+                                    <FormControlLabel value="D" control={<Radio />} label="Damaged"/>
+                                    <FormControlLabel value="N" control={<Radio />} label="No Seal"/>
+                                </RadioGroup>
+                            </FormControl>
+                            <TextField
+                            id="name"
+                            label="POD/Out Remarks"
+                            className={classes.textField}
+                            value="asdf"                                    
+                            margin="normal"
+                            />                                    
+                        </form>
+                    </div>
+                }
                 </main>
             </div>
         );        
