69bbd2a46cd8c073b6908372d9e908e37f578242
Bhaskar <bhaskarmuthineni@gmail.com>
2018-04-20 19:10:28 +0530

    BusyDialog changes

---
+++ dialog/BusyDialog.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Dialog from 'material-ui/Dialog';
+import CircularProgress from 'material-ui/Progress/CircularProgress';
+
+class BusyDialog extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const { classes, isLoading} = this.props;
+        return(
+            <Dialog className={classes.busyDialog} open={isLoading}>
+                <CircularProgress className={classes.progress} size={100} thickness={4} />
+            </Dialog>
+        );        
+    };
+}
+
+BusyDialog.propTypes = {
+    open: PropTypes.bool
+};
+
+export default BusyDialog;
